d69f69dffbe55897ca5c48df64e6d03bfb4c8b25
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-08 18:19:14 +0100

    new components - todo list & todo action

---
+++ src/app/todo-list/todo-action/todo-action.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-action',
+  templateUrl: './todo-action.component.html',
+  styleUrls: ['./todo-action.component.scss']
+})
+export class TodoActionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
