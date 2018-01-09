675bdf6b1e41f12440d0c094574a7b486511bd29
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-09 18:04:17 +0100

    nav component - filtering trough pipes and ngClass tab routing

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TodoService} from '../common/services/todo.service';
+import {TodoObj} from '../common/interfaces/todo.interface';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
-
-  constructor() { }
+export class NavComponent implements OnInit{
+  activeObj: TodoObj = {
+    active: true,
+    favourite: false,
+    completed: false
+  };
+  constructor(private _TodoService: TodoService) { }
 
   ngOnInit() {
+    this._TodoService.pipeValue = 'active';
+  }
+
+  handleClick(str: string) {
+    Object.keys(this.activeObj).map(i => this.activeObj[i] = false);
+    this.activeObj[str] = true;
+    this._TodoService.pipeValue = str;
   }
 
 }
