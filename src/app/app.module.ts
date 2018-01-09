2871c50b198335018047e0be757a79454bb8d91f
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-09 18:07:54 +0100

    main module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { CreateTodoComponent } from './create-todo/create-todo.component';
 import { TodoService } from './common/services/todo.service';
 import { TodoActionComponent } from './todo-list/todo-action/todo-action.component';
+import { TodoPipe } from './common/pipes/todo.pipe';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { TodoActionComponent } from './todo-list/todo-action/todo-action.compone
     NavComponent,
     TodoListComponent,
     CreateTodoComponent,
-    TodoActionComponent
+    TodoActionComponent,
+    TodoPipe
   ],
   imports: [
     BrowserModule,
