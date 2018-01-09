c60b181930d4ce28a41078f31515457b512cf87a
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-09 18:06:52 +0100

    todo action component - logic & styles

--- src/app/todo-list/todo-action/todo-action.component.ts
+++ src/app/todo-list/todo-action/todo-action.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {TodoObj} from '../../common/interfaces/todo.interface';
+import {TodoService} from '../../common/services/todo.service';
 
 @Component({
   selector: 'app-todo-action',
   templateUrl: './todo-action.component.html',
   styleUrls: ['./todo-action.component.scss']
 })
-export class TodoActionComponent implements OnInit {
+export class TodoActionComponent {
+  @Input() todoAction: TodoObj;
+  @Input() todoId: number;
+  constructor(private _TodoService: TodoService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  updateTodo(val: string) {
+    if ( val ) {
+      this.todoAction[val] = !this.todoAction[val];
+    } else {
+      this._TodoService.todoArray = this._TodoService.todoArray.filter(i => i.id !== this.todoId);
+    }
+  };
 }
