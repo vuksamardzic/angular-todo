483332dc82b42c1b2012adf5b07e13a6ab98c9d6
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-01-09 18:05:13 +0100

    create todo component - logic and style

--- src/app/create-todo/create-todo.component.ts
+++ src/app/create-todo/create-todo.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Todo} from '../common/interfaces/todo.interface';
+import {TodoService} from '../common/services/todo.service';
 
 @Component({
   selector: 'app-create-todo',
   templateUrl: './create-todo.component.html',
   styleUrls: ['./create-todo.component.scss']
 })
-export class CreateTodoComponent implements OnInit {
+export class CreateTodoComponent {
+  id = 0;
+  todoName: string;
+  todoArray: Todo[];
+  constructor(private _TodoService: TodoService) { }
 
-  constructor() { }
+  fillArray(ev: KeyboardEvent) {
+    if ( (ev === undefined || ev.keyCode === 13) && this.todoName !== '') {
+      const obj = {
+        id: this.id++,
+        name: this.todoName,
+        props: {
+          active: true,
+          completed: false,
+          favourite: false
+        }
+      };
 
-  ngOnInit() {
+      this._TodoService.todoArray.push(obj);
+      this.todoName = '';
+    }
   }
 
 }
